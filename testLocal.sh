bd1a5968315086da5285af1400f2d768518f0acf
raywoo32 <40863729+raywoo32@users.noreply.github.com>
2020-08-30 18:28:39 -0400

    Delete testLocal.sh

--- testLocal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash$TRINITY_DIR/Trinity --seqTypefq\
---left 104_RNA_R1.fastq.gz, WT_RNA_R1.fastq.gz \
---right 104_RNA_R2.fastq.gz, WT_RNA_R2.fastq.gz \
---SS_lib_typeRF \
---max_memory2G \
---CPU 2 \
---output 
